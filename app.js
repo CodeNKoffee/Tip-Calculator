8189345ec345b4718552259e0b740ac28b9c4e3d
Hatem Soliman <hatemthedev@gmail.com>
2022-07-03 17:51:23 +0200

    Removed unnecessary variables

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // VARIABLES
-const form = document.getElementById("form");
 const bill = document.getElementById("bill");
 const tipPercentage = document.getElementById("tip__percentage");
 const tipAmount = document.getElementById("tip__amount");
 const totalAmount = document.getElementById("total__amount");
-const empty = " ";
 
 
 // FUNCTIONS
